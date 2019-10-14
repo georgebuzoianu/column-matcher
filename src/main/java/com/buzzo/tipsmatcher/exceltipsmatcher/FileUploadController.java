4930e3b679d4f4e09b8f50cf7929f306109c8aac
George Buzoianu <buzoianu_george@yahoo.com>
2019-10-14 13:51:46 +0300

    Improve error handling

--- src/main/java/com/buzzo/tipsmatcher/exceltipsmatcher/FileUploadController.java
+++ src/main/java/com/buzzo/tipsmatcher/exceltipsmatcher/FileUploadController.java
@@ -7,9 +7,13 @@ import java.util.stream.Collectors;
 import cc.redberry.combinatorics.Combinatorics;
 import com.buzzo.tipsmatcher.exceltipsmatcher.model.Tipster;
 import org.apache.commons.lang3.tuple.Pair;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
@@ -23,39 +27,11 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import static java.util.stream.Collectors.*;
 
-//import hello.storage.StorageFileNotFoundException;
-//import hello.storage.StorageService;
-
 @Controller
 @RequestMapping("/file")
 public class FileUploadController {
 
-//    private final StorageService storageService;
-//
-//    @Autowired
-//    public FileUploadController(StorageService storageService) {
-//        this.storageService = storageService;
-//    }
-//
-//    @GetMapping("/")
-//    public String listUploadedFiles(Model model) throws IOException {
-//
-//        model.addAttribute("files", storageService.loadAll().map(
-//                path -> MvcUriComponentsBuilder.fromMethodName(FileUploadController.class,
-//                        "serveFile", path.getFileName().toString()).build().toString())
-//                .collect(Collectors.toList()));
-//
-//        return "uploadForm";
-//    }
-//
-//    @GetMapping("/files/{filename:.+}")
-//    @ResponseBody
-//    public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
-//
-//        Resource file = storageService.loadAsResource(filename);
-//        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
-//                "attachment; filename=\"" + file.getFilename() + "\"").body(file);
-//    }
+    private static Logger logger = LoggerFactory.getLogger(FileUploadController.class);
 
     @PostMapping("/upload")
     public String handleFileUpload(@RequestParam("excelFile") MultipartFile file,
@@ -67,14 +43,12 @@ public class FileUploadController {
         List<Tipster> tipstersList = getTipstersFromFile(file);
         redirectAttributes.addFlashAttribute("tipsters" , tipstersList);
 
-
+        logger.info("Tipsters found: {}", tipstersList.size());
         final Integer k = initK > tipstersList.size() ? tipstersList.size() : initK;
-
-        System.out.println(String.format("total found: %s and combine in sets of %s", tipstersList.size(), k));
 
         List<int[]> combinations = Combinatorics.combinations(tipstersList.size(), k).toList();
 
-
+        logger.info("Total no of combinations of {} in sets of {}: {}", tipstersList.size(), k, combinations.size());
 
         List<List<Tipster>> allCombinations = combinations.stream().map(combination ->
         {
@@ -85,7 +59,7 @@ public class FileUploadController {
             return tipstersCombination;
         }).collect(toList());
 
-        List<Pair<Integer, List<Tipster>>> intermediary = allCombinations.stream().map(tipstersCombination -> {
+        Map<Integer, List<List<Tipster>>> results = allCombinations.stream().map(tipstersCombination -> {
             int noOfSuccesses = 0;
             for (int i = 1; i <= 31; i++) {
                 final int day = i;
@@ -95,20 +69,19 @@ public class FileUploadController {
             }
             return Pair.of(noOfSuccesses, tipstersCombination);
         })
-        .filter(pair -> pair.getLeft()> 0)
-        .collect(Collectors.toList());
-
-
-        Map<Integer, List<List<Tipster>>> results = intermediary.stream().collect(groupingBy(Pair::getLeft, Collectors.mapping(Pair::getRight, Collectors.toList())));
+        .filter(pair -> pair.getLeft() > 1)
+        .collect(groupingBy(Pair::getLeft, Collectors.mapping(Pair::getRight, Collectors.toList())));
 
         Map<Integer, List<List<Tipster>>> resultsSorted = results.entrySet().stream()
                                                         .sorted(Map.Entry.comparingByKey(Comparator.reverseOrder()))
                                                         .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
                                                                 (oldValue, newValue) -> oldValue, LinkedHashMap::new));
 
-        redirectAttributes.addFlashAttribute("resultsSorted" , resultsSorted);
+        logger.info("Result map size: " + resultsSorted.size());
+        int noOfFoundCombinations =  resultsSorted.values().stream().mapToInt(list -> list.size()).sum();
 
-        System.out.println("tipsters found: " + tipstersList.size());
+        redirectAttributes.addFlashAttribute("noOfFoundCombinations" , noOfFoundCombinations);
+        redirectAttributes.addFlashAttribute("resultsSorted" , resultsSorted);
         return "redirect:/";
     }
 
@@ -117,18 +90,30 @@ public class FileUploadController {
         XSSFWorkbook workbook = new XSSFWorkbook(file.getInputStream());
         XSSFSheet worksheet = workbook.getSheetAt(0);
 
-        for (int i = 1; i < worksheet.getPhysicalNumberOfRows(); i++) {
+        for (int rowNum = 1; rowNum < worksheet.getPhysicalNumberOfRows(); rowNum++) {
 
 
-            XSSFRow row = worksheet.getRow(i);
+            XSSFRow row = worksheet.getRow(rowNum);
+
+            XSSFCell tipsterNameCell = row.getCell(0);
+
+            if(tipsterNameCell == null)
+                continue;
+
+            String tipsterName = tipsterNameCell.getStringCellValue();
 
             Tipster tipster = Tipster.builder()
-                    .name(row.getCell(0).getStringCellValue())
+                    .name(tipsterName)
                     .results(new HashMap())
                     .build();
+//            logger.info("Reading days for: {}", tipster.getName());
             for (int day = 1; day <= 31; day++) {
-                tipster.getResults().put(day, row.getCell(day)  !=null ? (int)row.getCell(day).getNumericCellValue() : 0);
 
+                int result = 0;
+                if(row.getCell(day) !=null && row.getCell(day).getCellType()== CellType.NUMERIC)
+                    result = (int)row.getCell(day).getNumericCellValue();
+
+                tipster.getResults().put(day, result);
             }
             tipstersList.add(tipster);
         }
